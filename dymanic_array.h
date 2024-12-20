f1ea4ec27737f5c8af47f35afe1d9041f7c24009
gleb <super.koio777@yandex.ru>
2024-12-20 20:40:27 +0300

    Update dynamic array defenition

--- dymanic_array.h
+++ dymanic_array.h
@@ -1,17 +1,22 @@
+#pragma once
+
 #define TYPE int
+#define INITIAL_LENGHT 100
+#define UPDATED_LENGHT 10
 
 struct Dynamic_array
 {
-    Node* node;
+    TYPE** dynamic_array;  // array of pointers to TYPE
+
+    unsigned char size;   // size of array
 };
 
-struct Node
-{
-    TYPE* data;
-};
+
 
 Dynamic_array* Dynamic_array_push_back(Dynamic_array* dynamic_array, TYPE* data); // push elem to the back
 
 Dynamic_array* Dynamic_array_change_elem(Dynamic_array* dynamic_array, unsigned char index, TYPE* data); // change data by index
 
 Dynamic_array* Dynamic_array_size(Dynamic_array* dynamic_array); // check size of an array
+
+Dynamic_array* Dynamic_array_resize(Dynamic_array* dynamic_array);
