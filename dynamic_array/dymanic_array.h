03ba5b4195f9135cc10a1678d5d0fba39247b4c0
gleb <super.koio777@yandex.ru>
2024-12-22 11:31:34 +0300

    move dynamic_array.h

---
+++ dynamic_array/dymanic_array.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define TYPE int
+#define INITIAL_LENGHT 100
+#define UPDATED_LENGHT 10
+
+struct Dynamic_array
+{
+    TYPE* array;  // array of pointers to TYPE
+
+    unsigned char available_size;   // size of array
+
+    unsigned char current_size;
+};
+
+typedef struct Dynamic_array Dynamic_array;
+
+Dynamic_array* Dynamic_array_create(); // create array of initial size
+
+void Dynamic_array_push_back(Dynamic_array* dynamic_array, TYPE data); // push elem to the back
+
+void Dynamic_array_change_elem(Dynamic_array* dynamic_array, unsigned char index, TYPE data); // change data by index
+
+unsigned char Dynamic_array_size(Dynamic_array* dynamic_array); // check size of an array
+
+void Dynamic_array_resize(Dynamic_array* dynamic_array); // resize array
